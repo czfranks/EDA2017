d76adf23fb4300810126ab17c8a162e43b6ede17
lmcsgit <gitlmcs@gmail.com>
2017-12-20 02:54:42 -0500

    crawler v0.00002 y links de carga

--- craw4.py
+++ craw4.py
@@ -24,36 +24,76 @@ def getLinks(url):
 #print( getLinks("https://elcomercio.pe/"))
 #print( getLinks("https://elcomercio.pe/peru") )
 
-quote_page = []
-quote_page = getLinks(url)
+namefile = "links.txt"
+def getLinkFromDisk(namefile):
+    listaLink=[]
+    file = open(namefile,"r")
+    msj=file.readline()
+    lista=list(msj.split(','))
+    #return lista
+    #return msj
+    for i in range(len(lista)):
+        listaLink.append(lista[i])
+    return listaLink
 
+
+quote_page=[]
+#quote_page.append(getLinkFromDisk(namefile))
+quote_page=getLinkFromDisk(namefile)
+
+# for a in quote_page:
+#     print a
+# print "FIN"
+#quote_page = []
+#quote_page = getLinks(url)
+#quote_page = ["https://elcomercio.pe/economia/peru/sector-construccion-seria-afectado-vacancia-presidencial-noticia-482656","https://elcomercio.pe/economia/peru/aeropuerto-chinchero-hay-acuerdo-compra-terreno-noticia-482608"]
+#quote_page = ['http://elcomercio.pe/elcomercio/politica/ppk-podra-asistido-abogado-jueves-noticia-482620','http://elcomercio.pe/elcomercio/politica/pedido-vacancia-ausencia-voto-cuentan-noticia-482635']
 # specify the url
 #quote_page = "https://elcomercio.pe/archivo"
 #quote_page = "http://www.bloomberg.com/quote/SPX:IND"
 #quote_page = ["http://www.bloomberg.com/quote/SPX:IND", "http://www.bloomberg.com/quote/CCMP:IND"]
 #quote_page = "https://elcomercio.pe/economia/peru/sector-construccion-seria-afectado-vacancia-presidencial-noticia-482656"
 #quote_page = ["https://elcomercio.pe/economia/peru/sector-construccion-seria-afectado-vacancia-presidencial-noticia-482656","https://elcomercio.pe/economia/peru/aeropuerto-chinchero-hay-acuerdo-compra-terreno-noticia-482608"]
+print "TIPO-> ",type(quote_page)
+print quote_page[0]
+# for a in quote_page:
+#     print a
+# print "FIN"
 
+# page = urllib2.urlopen(quote_page[1])
+# soup = BeautifulSoup(page, 'html.parser')
+# title_box = soup.find('h1',attrs={'class':'news-title'})
+# newtitles = title_box.text.encode('utf-8').strip().lower()
+# #newtitles = title_box.get_text()
+# print "TITULO: ",newtitles
+
+# #sumario
+# summary_box = soup.find('h2',attrs={'class':'news-summary'})
+# summaries = summary_box.text.lower()
+# print "SUMARIO: ",summaries
+#tam = len(quote_page)
+
+#for i in range (0,tam-1):
 for pg in quote_page:
     # query the website and return the html to the variable 'page'
     page = urllib2.urlopen(pg)
+    #page = urllib2.urlopen(quote_page[i])
 
     # parse the html using beautiful soup and store in variable soup
     soup = BeautifulSoup(page, 'html.parser')
 
     #titulo
     title_box = soup.find('h1',attrs={'class':'news-title'})
-    newtitles = title_box.text.encode('utf-8').strip()
+    newtitles = title_box.text.encode('utf-8').strip().lower()
     #newtitles = title_box.get_text()
     print "TITULO: ",newtitles
 
     #sumario
     summary_box = soup.find('h2',attrs={'class':'news-summary'})
-    summaries = summary_box.text
+    summaries = summary_box.text.lower()
     print "SUMARIO: ",summaries
 
-#name = name_box.text.strip() # strip() is used to remove starting and trailing
-#print name
+
 
 
 
